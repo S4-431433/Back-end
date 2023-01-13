15cc52828d8ea37d932ba6fe8d8ee7e4e6b6d78e
Jenson <431433@student.fontys.nl>
2023-01-13 22:34:24 +0100

    Admin data uit de database

---
+++ S4-BmBackend/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using S4_BmBackend.Logic;
+using S4_BmBackend.DTO;
+using S4_BmBackend.Data;
+
+namespace S4_BmBackend.Controllers
+{
+    [ApiController]
+    [Route("admin/[action]")]
+    public class AdminController : ControllerBase
+    {
+        AdminLogic adminLogic = new();
+        private readonly ILogger<AdminController> _logger;
+        public AdminController(ILogger<AdminController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult GetTopData()
+        {
+            return Ok(adminLogic.GetData());
+        }
+    }
+}
---
+++ S4-BmBackend/Logic/AdminLogic.cs
@@ -0,0 +1,41 @@
+﻿using S4_BmBackend.Data;
+
+namespace S4_BmBackend.Logic
+{
+    public class AdminLogic
+    {
+        Account account = new();
+        Product product = new();
+        Trainer trainer = new();
+         public List<int> GetData()
+        {
+            List<int> data = new();
+            data.Add(GetUserCount());
+            data.Add(GetTrainerCount());
+            data.Add(GetProductCount());
+            data.Add(GetRevenue());
+            return data;
+        }
+
+
+        public int GetUserCount()
+        {
+            return account.GetAll().Count;
+        }
+
+        public int GetTrainerCount()
+        {
+            return trainer.GetAll().Count;
+        }
+
+        public int GetProductCount()
+        {
+            return product.GetAll().Count;
+        }
+
+        public int GetRevenue()
+        {
+            return 21;
+        }
+    }
+}
